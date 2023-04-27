790361a0593076647e158fbb3db9498fc528cfec
Mohamedbourek <87719740+Mohamedbourek@users.noreply.github.com>
2023-04-27 22:20:09 +0100

    Add files via upload

--- Admin/gestionPack.php
+++ Admin/gestionPack.php
@@ -8,13 +8,15 @@
 </head>
 <body>
 	<?php 
+	
+	
 	 function afficherPack()
 	 {
+	session_start();
 	 $server="localhost";
      $nom_bdd="essai";
      $user="root";
      $password="";
-	 $date = date("Y-m-d H:i:s");
 		try{
 		   $connexion = new PDO("mysql:host=$server;dbname=$nom_bdd",$user,$password);
 		   $req = "SELECT * FROM PACK ORDER BY NUMEROPACK ASC";
@@ -22,19 +24,45 @@
 		   while($tuple = $res->fetch(PDO::FETCH_ASSOC)){
 			echo "<tr>";
 			echo "<td>" . $tuple["NUMEROPACK"] . "</td>";
+			echo "<td>" . $tuple["NOMPACK"] . "</td>";
 			echo "<td>" . $tuple["CATEGORIE"] . "</td>";
 			echo "<td>" . $tuple["WILAYA"] . " </td>";
-			echo "<td>" . $date . " </td>";
+			echo "<td>" . $tuple["TYPE_PACK"] . " </td>";
+			echo "<td>" . $tuple["DATE_CREATION"] . " </td>";
 			echo "<td>" . $tuple["PRIX"] . " DA</td>";
+			
+			if(isset($_SESSION["heberg"]) && $_SESSION["nomPack"]==$tuple["NOMPACK"]) 
+			{$heberg = $_SESSION["heberg"];echo "<td>" . $heberg. " </td>";}
+			else
+			{$heberg = "Not Included";echo "<td>" . $heberg. " </td>";}
+
+			if(isset($_SESSION["transpo"]) && $_SESSION["nomPack"]==$tuple["NOMPACK"]) 
+			{$transpo = $_SESSION["transpo"];echo "<td>" . $transpo. " </td>";}
+			else
+			{$transpo = "Not Included";echo "<td>" . $transpo. " </td>";}
+
+			if(isset($_SESSION["food"]) && $_SESSION["nomPack"]==$tuple["NOMPACK"]) 
+			{$food = $_SESSION["food"];echo "<td>" . $food. " </td>";}
+			else
+			{$food = "Not Included";echo "<td>" . $food. " </td>";}
+
+			if(isset($_SESSION["guide"]) && $_SESSION["nomPack"]==$tuple["NOMPACK"]) 
+			{$guide = $_SESSION["guide"];echo "<td>" . $guide. " </td>";}
+			else
+			{$guide = "Not Included";echo "<td>" . $guide. " </td>";}
+
+
+
 			echo "</tr>";
     		}
-			
+		
 		   
 	   }
 	   catch (PDOException $e) 
    {
 	   echo "Erreur ! " . $e->getMessage() . "<br/>";
-   }		
+   }
+			
 		
     }
 	?>
@@ -43,19 +71,26 @@
 		<thead>
 			<tr>
 				<th>Numéro du pack</th>
+				<th>Nom du pack</th>
 				<th>Catégorie</th>
 				<th>Wilaya</th>
+				<th>Type Du Pack</th>
 				<th>Date de création</th>
                 <th>Prix</th>
+				<th>With Héberg</th>
+				<th>With transport</th>
+				<th>With food</th>
+				<th>With guide</th>
+				
 			</tr>
 		</thead>
 		<tbody>
-			 <?php afficherPack(); ?>
+			 <?php  afficherPack(); ?>
 		</tbody>
 	</table>
-	<a href="créationPack.html"><button>Créer un pack</button></a>
+	
+	<a href="créationPack.php"><button>Créer un pack</button></a>
 	<a href='modificationPack.html'><button>Modifier un pack</button></a>
 	<a href='suppressionPack.html'><button>Supprimer un pack</button></a>
-
 </body>
 </html>
